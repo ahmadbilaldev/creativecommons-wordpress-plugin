630ad7def36a9f3b6a26b6f35c63f11870662aba
Ahmad Bilal <ahmadbilalme@gmail.com>
2019-06-23 21:18:31 +0500

    Fix Color Settings

--- src/CC-BY/block.js
+++ src/CC-BY/block.js
@@ -26,12 +26,12 @@ const { ColorPalette } = wp.editor;
  * @return {?WPBlock}          The block, if it has been successfully
  *                             registered; otherwise `undefined`.
  */
-registerBlockType( 'cgb/cc-by', {
+registerBlockType('cgb/cc-by', {
 	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __( 'CC-BY' ), // Block title.
+	title: __('CC-BY'), // Block title.
 	icon: 'media-text', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
 	category: 'cc-licenses', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
-	keywords: [ __( 'creative commons' ), __( 'CC-BY' ), __( 'attribution' ) ],
+	keywords: [__('creative commons'), __('CC-BY'), __('attribution')],
 
 	// Attributes
 	attributes: {
@@ -56,7 +56,7 @@ registerBlockType( 'cgb/cc-by', {
 	 *
 	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
 	 */
-	edit: function( props ) {
+	edit: function (props) {
 		const background_color = props.attributes.background_color; // To bind button background color
 		// Style object for the button
 		// I created a style in JSX syntax to keep it here for
@@ -69,27 +69,28 @@ registerBlockType( 'cgb/cc-by', {
 			fontSize: '16px',
 		};
 		// onChange event functions
-		function onChangeBgColor( content ) {
-			props.setAttributes( { background_color: content } );
+		function onChangeBgColor(content) {
+			props.setAttributes({ background_color: content });
 		}
 
 		// Creates a <p class='wp-block-cgb-block-cc-block'></p>.
 		return [
 			<InspectorControls>
 				<PanelColorSettings
-					title={ __( 'Color Settings', 'creativecommons' ) }
-					colorSettings={ [
+					title={__('Color Settings', 'creativecommons')}
+					colorSettings={[
 						{
+							value: onChangeBgColor,
 							onChange={ onChangeBgColor }, // onChange event callback
-							label: __( 'Background Color' ),
+							label: __('Background Color'),
 						},
-					] }
+					]}
 				/>
 			</InspectorControls>,
-			<div className={ props.className } style={ block_style }>
+			<div className={props.className} style={block_style}>
 				<img src="https://licensebuttons.net/l/by/3.0/88x31.png" alt="CC-BY" />
 				<p>
-					This blog post is licensed under a{ ' ' }
+					This blog post is licensed under a{' '}
 					<a href="https://creativecommons.org/licenses/by/4.0/">
 						Creative Commons Attribution 4.0 International license.
 					</a>
@@ -106,15 +107,15 @@ registerBlockType( 'cgb/cc-by', {
 	 *
 	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
 	 */
-	save: function( props ) {
+	save: function (props) {
 		const block_style = {
 			backgroundColor: props.attributes.background_color,
 		};
 		return (
-			<div style={ block_style }>
+			<div style={block_style}>
 				<img src="https://licensebuttons.net/l/by/3.0/88x31.png" alt="CC" />
 				<p>
-					This blog post is licensed under a{ ' ' }
+					This blog post is licensed under a{' '}
 					<a href="https://creativecommons.org/licenses/by/4.0/">
 						Creative Commons Attribution 4.0 International license.
 					</a>
@@ -122,4 +123,4 @@ registerBlockType( 'cgb/cc-by', {
 			</div>
 		);
 	},
-} );
+});
